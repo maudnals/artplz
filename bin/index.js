d8ba774631f3de2cceb3c94f331681a72db021ef
maudnals <maud.nalpas@gmail.com>
2020-08-20 21:54:42 +0200

    feat: added basic image printing + artist input

--- bin/index.js
+++ bin/index.js
@@ -1,11 +1,29 @@
 const term = require("terminal-kit").terminal;
-term.green.bold("Artist?\n");
+const validator = require("validator");
 
-term.drawImage(
-  "https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/Monet_-_Impression%2C_Sunrise.jpg/400px-Monet_-_Impression%2C_Sunrise.jpg",
-  { shrink: { width: term.width, height: term.height } }
-);
-// term.inputField(function (error, input) {
-//   term.green("\nYour name is '%s'\n", input);
-// });
-// term.red("Artist?\n");
+(async function () {
+  await term.slowTyping("Artist?\n", { delay: 90 });
+  const name = await term.inputField().promise;
+  const nameChunks = name.split(" ").map((chunk) => chunk.trim());
+  nameChunks.forEach((chunk) => {
+    if (!validator.isAlpha(chunk) || validator.isEmpty(chunk)) {
+      term("\nOh no, that's empty or contains special characters 😳\n");
+      term("\nTry again!\n", { delay: 90 });
+      process.exit();
+    }
+  });
+
+  term.green(
+    "\nLooking for %s's good stuff\n",
+    nameChunks
+      .map((chunk) => `${chunk.charAt(0).toUpperCase()}${chunk.slice(1)}`)
+      .join(" ")
+  );
+  await term.slowTyping("¸.·´¯`·.¸\n", { delay: 90 });
+  await term.slowTyping("Monet Impression Sunrise \n", { delay: 90 });
+  await term.drawImage(
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/Monet_-_Impression%2C_Sunrise.jpg/400px-Monet_-_Impression%2C_Sunrise.jpg",
+    { shrink: { width: term.width, height: term.height } }
+  );
+  process.exit();
+})();
