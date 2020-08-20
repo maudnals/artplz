bdb403b9e164c929508c829e6eea57205a13e050
maudnals <maud.nalpas@gmail.com>
2020-08-20 23:19:24 +0200

    feat: added wikipedia scrapping

--- bin/index.js
+++ bin/index.js
@@ -1,29 +1,84 @@
 const term = require("terminal-kit").terminal;
 const validator = require("validator");
+const fetch = require("node-fetch");
+// TODO remove
+const cheerio = require("cheerio");
+// bug: mondrian, gauguin, rubens
+// OK: degas, picasso, kahlo, klimt
+// TODO add console.log
+// TODO display title
+
+function validateString(str) {
+  return validator.isAlpha(str) && !validator.isEmpty(str);
+}
 
 (async function () {
-  await term.slowTyping("Artist?\n", { delay: 90 });
+  term.green("Artist?\n");
   const name = await term.inputField().promise;
   const nameChunks = name.split(" ").map((chunk) => chunk.trim());
   nameChunks.forEach((chunk) => {
-    if (!validator.isAlpha(chunk) || validator.isEmpty(chunk)) {
+    if (!validateString(chunk)) {
       term("\nOh no, that's empty or contains special characters 😳\n");
       term("\nTry again!\n", { delay: 90 });
       process.exit();
     }
   });
 
-  term.green(
-    "\nLooking for %s's good stuff\n",
-    nameChunks
-      .map((chunk) => `${chunk.charAt(0).toUpperCase()}${chunk.slice(1)}`)
-      .join(" ")
-  );
-  await term.slowTyping("¸.·´¯`·.¸\n", { delay: 90 });
-  await term.slowTyping("Monet Impression Sunrise \n", { delay: 90 });
-  await term.drawImage(
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/Monet_-_Impression%2C_Sunrise.jpg/400px-Monet_-_Impression%2C_Sunrise.jpg",
-    { shrink: { width: term.width, height: term.height } }
-  );
+  const fullDisplayName = nameChunks
+    .map((chunk) => `${chunk.charAt(0).toUpperCase()}${chunk.slice(1)}`)
+    .join(" ");
+
+  const urlName = nameChunks.join("_");
+
+  term.green("\nLooking for %s's good stuff\n", fullDisplayName);
+
+  const u = await fetch(`https://en.wikipedia.org/wiki/${urlName}`)
+    .then((response) => {
+      return response.text();
+    })
+    .then((html) => {
+      const htmlShort = html.substring(0, 20000);
+      const artworkListStartIdx = htmlShort.indexOf("Notable work");
+      const artworksHtml = htmlShort.substring(
+        artworkListStartIdx,
+        artworkListStartIdx + 650
+      );
+
+      // TODO
+      // in case we overflow this table like for degas
+      //   const blaaa = artworksHtml.indexOf("</tr>");
+      //   console.log(blaaa);
+
+      const artworkWikiPagePaths = artworksHtml
+        // split to distinguish artworks
+        .split('<a href="')
+        // this guarantees that it's likely to be an artwork AND that the link isn't truncated
+        .filter((entry) => entry.includes("title"))
+        .filter((entry) => entry.startsWith("/wiki/"))
+        // clean to keep only the page path (href)
+        .map((entry) => entry.substring(0, entry.indexOf(`"`)));
+      const randomIdx = Math.floor(Math.random() * artworkWikiPagePaths.length);
+      const selectedArtworkWikiPagePath = artworkWikiPagePaths[randomIdx];
+      fetch(`https://en.wikipedia.org${selectedArtworkWikiPagePath}`)
+        .then((response) => {
+          return response.text();
+        })
+        .then(async (html) => {
+          const htmlShort = html.substring(0, 20000);
+          const imgStartIdx = htmlShort.indexOf("<img");
+          const imgEl = htmlShort.substring(imgStartIdx, imgStartIdx + 400);
+          const imgAttributes = imgEl.split(" ");
+          const srcStr = imgAttributes.find((attr) => attr.startsWith("src="));
+          const imgSrc = srcStr.substring(
+            `src="`.length,
+            srcStr.length - `"`.length
+          );
+          const x = await term.drawImage(`https:${imgSrc}`, {
+            shrink: { width: term.width * 2, height: term.height * 1.4 },
+          });
+        });
+    });
+
+  await term.slowTyping("¸.·´¯`·.¸\n", { delay: 100 });
   process.exit();
 })();
