96862374ccd5be0a8211d609ed981c1120d456ad
maudnals <maud.nalpas@gmail.com>
2021-05-28 18:33:04 +0200

    feat: added nonstop mode

--- bin/index.js
+++ bin/index.js
@@ -9,31 +9,33 @@ const { getArtworkWikiPagePath, getImgSrc } = require('./scraper');
 const { logArtworkAndCaption, logWait, logBye, logError } = require('./logger');
 const { getArtistName } = require('./input');
 
-(async function () {
-  try {
-    const artistNameInput = await getArtistName();
+async function main() {
+  const artistNameInput = await getArtistName();
     let artistName =
       artistNameInput.trim() || getRandomArtist(config.randomArtists);
     if (!artistName) {
       const { randomArtists } = config;
       artistName = getRandomArtist(randomArtists);
-    }
-    const artistNameChunks = artistName
-      .trim()
-      .split(' ')
-      .map((chunk) => chunk.trim())
-      .filter((chunk) => chunk);
+  }
+  const artistNameChunks = artistName
+    .trim()
+    .split(' ')
+    .map((chunk) => chunk.trim())
+    .filter((chunk) => chunk);
+  await logWait();
+  const artworkWikiPagePath = await getArtworkWikiPagePath(artistNameChunks);
+  const artworkTitle = await urlPathToArtworkTitle(artworkWikiPagePath);
+  const imgSrc = await getImgSrc(artworkWikiPagePath);
+  const artistDisplayName = artistNameChunksToDisplayName(artistNameChunks);
+  await logArtworkAndCaption(imgSrc, artworkTitle, artistDisplayName);
+}
 
-    await logWait();
-    const artworkWikiPagePath = await getArtworkWikiPagePath(artistNameChunks);
-    const artworkTitle = await urlPathToArtworkTitle(artworkWikiPagePath);
-    const imgSrc = await getImgSrc(artworkWikiPagePath);
-    const artistDisplayName = artistNameChunksToDisplayName(artistNameChunks);
-    await logArtworkAndCaption(imgSrc, artworkTitle, artistDisplayName);
-    await logBye();
-    process.exit();
-  } catch (error) {
-    logError(error);
-    process.exit();
+(async function () {
+  while (true) {
+    try {
+      await main();
+    } catch (error) {
+      logError(error);
+    }
   }
 })();
